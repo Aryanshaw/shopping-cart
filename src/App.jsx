ba8bddd8abe2cdc367facc96ee45c9d7661f188d
Aryan <aryanrot234@gmail.com>
2022-03-12 02:20:15 +0530

    updated link

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
 import Cart from "./pages/Cart";
-import React from 'react';
+import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
 import Error from "./pages/Error";
 import Register from "./pages/Register";
 import SingleProduct from "./pages/SingleProduct";
-import {  Route , Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-
-const App =() => {
-
-
+const App = () => {
   return (
     <>
-    <Routes>
-      <Route exact path='/' element={<Home/>} />
-      <Route exact path='/productlist' element={<ProductList/>} />
-      <Route path='/singleproduct' element={<SingleProduct/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register/>} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path="*" element={<Error/>} />
-
-    </Routes>
-    {/* <Home/>
+      <Routes>
+        <Route exact path="/shopping-cart" element={<Home />} />
+        <Route exact path="/productlist" element={<ProductList />} />
+        <Route path="/singleproduct" element={<SingleProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      {/* <Home/>
     <Cart/> */}
     </>
-    );
-       
-    
-    
+  );
 };
 
 export default App;
