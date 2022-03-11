84024cec7654c288a588234f08914f569ec98f30
Aryan <aryanrot234@gmail.com>
2022-03-12 02:22:30 +0530

    updated link

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Btn = styled.button`
   margin-left: 20px;
   margin-top: 20px;
   padding: 20px 40px 20px 40px;
-  
-`
+`;
 
 const Error = () => {
   return (
     <>
-    <h1>Oops ! page not found</h1>
-    <Link to="/">
-    <Btn>HOME</Btn>
-    </Link>
+      <h1>Oops ! page not found</h1>
+      <Link to="/shopping-cart">
+        <Btn>HOME</Btn>
+      </Link>
     </>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
